bf1e0d553b31a39e5dd22c3ebf236fbae1f03eeb
Akram Ahamed <akram.ahammad@gmail.com>
2022-08-12 00:43:37 +0530

    Added validation for login form using template forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: [
+  './src/**/*.{html,ts}',
+  './src/**/**/*.{html,ts}',
+  '*.{html,ts}'
+],
+  safelist:['bg-red-400','bg-blue-400','bg-green-400'],
   theme: {
     extend: {},
   },
