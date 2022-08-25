356ee2344252150244ef6b5fe12551f18c8b9afc
Akram Ahamed <akram.ahammad@gmail.com>
2022-08-25 23:48:07 +0530

    Added cypress test for playing video player

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,6 @@
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+  it('Sanity test', () => {
     cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
+    cy.contains('#header .text-3xl','Clips')
   })
 })
