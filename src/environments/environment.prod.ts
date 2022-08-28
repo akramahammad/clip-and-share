4b8244518ca587974db1b0731ed1ff98ce4c70df
Akram Ahamed <akram.ahammad@gmail.com>
2022-08-28 11:25:02 +0530

    Added firebase config to environment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBzC9_dtHpEYdTJ-nUOPjjt_j7Fuo5KIAs",
+    authDomain: "clips-fb5c8.firebaseapp.com",
+    projectId: "clips-fb5c8",
+    storageBucket: "clips-fb5c8.appspot.com",
+    appId: "1:773631897223:web:ea128e73e20428f2399222"
+  }
 };
